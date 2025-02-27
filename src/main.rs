7cd77a36a17db8b8b4c134926dcd82637cfe97e1
jagoum <jagoum24@gmail.com>
2025-02-27 16:30:46 +0100

    Added function to extract numbers from a pull request

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let enable_fib = args.get(1).map_or(false, |arg| arg == "true");
-    let _max_threshold: u128 = args.get(3).and_then(|arg| arg.parse().ok()).unwrap_or(100);
+    let max_threshold: u128 = args.get(3).and_then(|arg| arg.parse().ok()).unwrap_or(100);
     let users_input: u128 = args.get(2).and_then(|args| args.parse().ok()).unwrap_or(0);
-    let max_threshold = extract_nums("Hello I will 23.8 like to give you 50.0 thousand");
-    let max_threshold = max_threshold[0] as u128;
+    // let max_threshold = extract_nums("Hello I will 23.8 like to give you 50.0 thousand");
+
+    // let max_threshold = max_threshold[0] as u128;
+    
     println!("The Fibbot Enable_Status: {}",enable_fib);
     println!("The maximum threshold Value is : {}",max_threshold);
     println!("The users input is : {users_input}");
+
 
     if enable_fib && users_input <= max_threshold {
         println!("Printing fibonaci Series up to {users_input}th index");
