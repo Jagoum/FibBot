21d4d3ca56f5257c3bd45725336ba30f033feba9
jagoum <jagoum24@gmail.com>
2025-02-28 19:26:45 +0100

    Added something

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod extract_nums;
 mod get_pull_request;
+mod push_comment;
 mod fibbonacci_calculator;
 use std::env;
 use get_pull_request::get_pr;
@@ -58,42 +59,7 @@ Ok(())
 
 
 
-/// This function get the content from a pull request and then parse it to extract numbers
 
-
-
- 
- pub async fn post_comment(pr_content: &str) -> Result<(), reqwest::Error> {
-     let repo = env::var("GITHUB_REPOSITORY").expect("GITHUB_REPOSITORY not set");
-     let pr_number = env::var("PR_NUMBER")
-         .expect("PR_NUMBER not set")
-         .parse::<u32>()
-         .expect("Invalid PR_NUMBER");
-        
-     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
-     
-     let url = format!(
-         "https://api.github.com/repos/{}/issues/{}/comments",
-         repo, pr_number
-        );
-        
-        let client = Client::new();
-        let response = client
-         .post(&url)
-         .header("Authorization", format!("Bearer {}", github_token))
-         .header("User-Agent", "FibBot")
-         .header("Accept", "application/vnd.github.full+json")
-         .json(&serde_json::json!({ "body": pr_content }))
-         .send()
-         .await?;
- 
-     if response.status().is_success() {
-         println!("✅ Comment posted successfully.");
-     } else {
-         eprintln!("❌ Failed to post comment: {:?}", response.text().await?);
-     }
-     Ok(())
-    }
 
 
 
