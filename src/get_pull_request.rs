8a6bf56002ccef259f56eca214fa55bf93efe394
jagoum <jagoum24@gmail.com>
2025-03-01 14:44:55 +0100

    Added some new features

--- src/get_pull_request.rs
+++ src/get_pull_request.rs
@@ -2,10 +2,10 @@ use crate::extract_nums;
 
 pub async fn get_pr(pr_num:u32) -> Vec<u128>{
 
-    let files = octocrab::instance().pulls("Jagoum", "FibBot").list_files(pr_num.into()).await;
+    let files = octocrab::instance().pulls("jagoum", "FibBot").list_files(pr_num.into()).await;
     let files = files.unwrap().items.first().unwrap().patch.clone().unwrap();
     println!("Pull Resquest Contents:\n{}",files);
-    let nums = extract_nums(&files.as_str());
+    let nums = extract_nums(files.as_str());
     println!("Collected Nums: {nums:?}");
     nums
     
