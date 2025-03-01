7fff470029944f821fb197c7f29b56082cacc1ac
jagoum <jagoum24@gmail.com>
2025-03-02 00:29:10 +0100

    Resolved conflict

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ This project is to learn how to  use github apis and to familiarise myself with 
 
 This project scans  a pull request for numbers and post the result as a comment on github . 
 
-The numbers are parse into a fibbonacci calculator and the post the result of the fibbonacci calculator to this gh repo as a comment
+The numbers are parse into a fibbonacci calculator and then post the result of the fibbonacci calculator to this gh repo as a comment
 
 I used some rust libaries like octocrab to get the content of a pull request
 
 I used regex pattern matching to extract the the numbers from the pull request that has being parsed to a string
 
 To parse the string i called some specail methods on the the content of the pull request.
+
+I Love this chanlenge
 
 ```rs
 
--- src/main.rs
+++ src/main.rs
@@ -7,68 +7,60 @@ use get_pull_request::get_pr;
 use extract_nums::extract_nums;
 
 use fibbonacci_calculator::fibonacci;
-use push_comment::post_comment;
+use push_comment::post_github_comment;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-
     // let repo = env::var("GITHUB_REPOSITORY").expect("GITHUB_REPOSITORY not set");
-    let pr_number = env::var("PR_NUMBER")
-        .expect("PR_NUMBER not set")
-        .parse::<u32>()
-        .expect("Invalid PR_NUMBER");
+    // println!("Hello, world!\n{:?}", extract_nums("12.9we 12.0 ,90 72"));
 
-    // println!("Hello, world!");
-    
     let args: Vec<String> = env::args().collect();
 
-    let enable_fib = args.get(1).map_or(false, |arg| arg == "true");
-    let max_threshold: u128 = args.get(3).and_then(|arg| arg.parse().ok()).unwrap_or(100);
-    let users_input: u128 = args.get(2).and_then(|args| args.parse().ok()).unwrap_or(0);
+    let github_token = args.get(1).and_then(|token| Some(token.as_str())).unwrap_or("default");
+    let enable_fib = args.get(2).map_or(true, |arg| arg == "true");
+    let users_input: u128 = args.get(3).and_then(|args| args.parse().ok()).unwrap_or(0);
+    let max_threshold: u128 = args.get(4).and_then(|arg| arg.parse().ok()).unwrap_or(187);
+    let pr_number = args.get(5).and_then(|new| new.parse().ok()).unwrap_or(2);
+    
     // let max_threshold = extract_nums("Hello I will 23.8 like to give you 50.0 thousand");
-
     // let max_threshold = max_threshold[0] as u128;
     
     println!("The Fibbot Enable_Status: {}",enable_fib);
     println!("The maximum threshold Value is : {}",max_threshold);
     println!("The users input is : {users_input}");
 
+    // user input is almost useless just for fun
 
-    if enable_fib && users_input <= max_threshold {
-        println!("Printing fibonaci Series up to {users_input}th index");
-        for (_, element) in fibonacci(users_input.try_into().unwrap()).iter().enumerate(){
-            print!("{} ",element);
-        }
+    if enable_fib {
+        // println!("Testing \nPrinting fibonaci Series up to {users_input}th index");
+        // for (_, element) in fibonacci(max_threshold).iter().enumerate(){
+        //     print!("{} ",element);
+        // }
+        println!();
+            // let nums = get_pr(pr_number);
+            // Here am converting the output of the fibonacci of those multiple numbers into a string 
+            // This is so that i can parse it to my post comment which takes an &str
+            // So here i use nested for loops which is not really the best
+            let mut string: String = String::from("##Pull Content: ");
+            let response = get_pr(pr_number).await;
+           
+            println!("Content to be Posted:");
+
+            for &num in &response{
+                
+                let fib = fibonacci(num);
+                string.push_str(format!("- Fibonacci({}) = {:?}\n", num, fib).as_str());
+                
+            }
+            
+            let _posted_content = post_github_comment(github_token,"Jagoum","FibBot",pr_number,&string);
+    
+        // Here am passing the string as parameter into this funcition that posts to github 
+        //This string contains the results of our fibo sequence of the numbers we collected
+            
     }
 
-    // let nums = get_pr(pr_number);
-    // Here am converting the output of the fibonacci of those multiple numbers into a string 
-    // This is so that i can parse it to my post comment which takes an &str
-    // So here i use nested for loops which is not really the best
-    let mut string: String = String::from("##Pull Content: ");
-    let response = get_pr(pr_number).await;
-   
-    for &num in &response{
-        let fib = fibonacci(num);
-        string.push_str(format!("- Fibonacci({}) = {:?}\n", num, fib).as_str());
-    }
-    
-
-// Here am passing the string as parameter into this funcition that posts to github 
-//This string contains the results of our fibo sequence of the numbers we collected
-    let posted_content = post_comment(string.as_str());
-
-
-    println!("Content to be Posted\n{:?}",posted_content.await.unwrap());
-    
 Ok(())
 }
-
-
-
-
-
-
-
